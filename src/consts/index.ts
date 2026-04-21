42b80f312fbb7486d87a6a66b5d751b9c1f10136
Adi Setyono <adi.setyono@gmail.com>
2026-04-21 12:37:09 +0700

    update env var name

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,5 +1,5 @@
 export const baseUrl = process.env.APP_BASE_URL ?? 'http://localhost:3000';
 export const redirectUrl = process.env.APP_REDIRECT_URL ?? `${baseUrl}/callback`;
 
-export const appId = process.env.LOGTO_APP_ID ?? 'sampleId'; // Register the sample app in Logto dashboard
-export const endpoint = process.env.LOGTO_ENDPOINT ?? 'http://localhost:3001'; // Replace with your own Logto endpoint
+export const appId = process.env.SSO_APP_ID ?? 'sampleId'; // Register the sample app in Logto dashboard
+export const endpoint = process.env.SSO_ENDPOINT ?? 'http://localhost:3001'; // Replace with your own Logto endpoint
--- src/include.d/env.d.ts
+++ src/include.d/env.d.ts
@@ -2,8 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly APP_BASE_URL?: string;
     readonly APP_REDIRECT_URL?: string;
-    readonly LOGTO_APP_ID?: string;
-    readonly LOGTO_ENDPOINT?: string;
+    readonly SSO_APP_ID?: string;
+    readonly SSO_ENDPOINT?: string;
   }
 }
 
